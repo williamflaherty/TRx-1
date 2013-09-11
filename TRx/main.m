c9640802a48de9ad9a1f86fb6964053f27ef151c
Mark Bellott <mdbellott@gmail.com>
2013-09-11 00:40:46 -0400

    First Commit!

---
+++ TRx/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TRx
+//
+//  Created by Mark Bellott on 9/11/13.
+//  Copyright (c) 2013 Team Haiti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TRAppDelegate class]));
+    }
+}

3db56467cdc43b85b250c59bccab758411e88d3b
Mark Bellott <mdbellott@gmail.com>
2013-09-22 15:19:54 -0400

    PatientList Cells

---
+++ ViewControllers/PatientList/TRPatientListCell.h
@@ -0,0 +1,13 @@
+//
+//  TRPatientListCell.h
+//  TRx
+//
+//  Created by Mark Bellott on 9/22/13.
+//  Copyright (c) 2013 TeamHaiti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TRPatientListCell : UITableViewCell
+
+@end
---
+++ ViewControllers/PatientList/TRPatientListCell.m
@@ -0,0 +1,29 @@
+//
+//  TRPatientListCell.m
+//  TRx
+//
+//  Created by Mark Bellott on 9/22/13.
+//  Copyright (c) 2013 TeamHaiti. All rights reserved.
+//
+
+#import "TRPatientListCell.h"
+
+@implementation TRPatientListCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end

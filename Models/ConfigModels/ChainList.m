841b823b3bf4bc394b9bd25b10b9cf4c91f98a98
John Cotham <jcotham@utk.edu>
2014-01-12 18:55:46 -0500

    ChainList now has a to-many relationship in model

--- Models/ConfigModels/ChainList.m
+++ Models/ConfigModels/ChainList.m
@@ -14,4 +14,18 @@
 @dynamic name;
 @dynamic chains;
 
+/*
++(NSOrderedSet *)getStackListFromContext:(NSManagedObjectContext *)context {
+    NSFetchRequest *fetchRequest = [[[context persistentStoreCoordinator] managedObjectModel] fetchRequestTemplateForName:@"StackList"];
+    NSError *error = nil;
+    NSArray *fetchedObjects = [context executeFetchRequest:fetchRequest error:&error];
+    if (fetchedObjects == nil) {
+        NSLog(@"Error retrieving %@ list: %@", @"StackList", error);
+        return nil;
+    }
+    ChainList *list = [fetchedObjects objectAtIndex:0];
+    
+    return list.chains;
+}*/
+
 @end
